83777c92cb4b963cac4bd7d38a19a2a21be29be7
Tom Valk <tomvalk@gmail.com>
2015-12-07 13:41:44 +0100

    Java compatibility upped to 1.8.

--- src/nl/hu/coproco/Main.java
+++ src/nl/hu/coproco/Main.java
@@ -1,10 +1,7 @@
 package nl.hu.coproco;
 
-/**
- * Created by tom on 7-12-2015.
- */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Starting CoProCo...");
     }
 }
