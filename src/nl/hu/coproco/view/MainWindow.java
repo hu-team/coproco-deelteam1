25fe8e22111820369fb0778a111084ce5b538726
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-08 11:55:14 +0100

    src/main/resources aangemaakt met daarin de view(fxml).

--- src/nl/hu/coproco/view/MainWindow.java
+++ src/nl/hu/coproco/view/MainWindow.java
@@ -1,6 +1,9 @@
 package nl.hu.coproco.view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
@@ -9,13 +12,20 @@ public class MainWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setResizable(false);
+        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("editor/editorScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/editorScene.fxml"));
+        //EditorController editorController = (EditorController) fxmlLoader.getController();
+
+        //primaryStage.setResizable(false);
         primaryStage.setTitle("CoProCo");
-//        primaryStage.setScene();
+
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.sizeToScene();
         primaryStage.show();
     }
 
     public void openWindow(String args[]) {
         launch(args);
     }
+
 }
