6e468f366f9da3b56c3df007d0bf2cc11dc15176
Tom Valk <tomvalk@gmail.com>
2015-12-08 14:59:07 +0100

    Purpose and Scope storage.

---
+++ src/main/java/nl/hu/coproco/domain/ScopeStorage.java
@@ -0,0 +1,31 @@
+package nl.hu.coproco.domain;
+
+import java.util.ArrayList;
+
+public class ScopeStorage {
+    private static ScopeStorage instance;
+
+    private ArrayList<Scope> scopeStorage;
+
+    public static ScopeStorage getInstance() {
+        if (instance == null) {
+            instance = new ScopeStorage();
+        }
+        return instance;
+    }
+
+
+    public ScopeStorage() {
+        this.scopeStorage = new ArrayList<Scope>();
+
+        this.initDefaultScopes();
+    }
+
+    private void initDefaultScopes() {
+        // TODO: Default inserts
+    }
+
+    public ArrayList<Scope> getScopes() {
+        return this.scopeStorage;
+    }
+}
