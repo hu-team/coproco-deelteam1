0f29fdad7892a555afccc5eae54f60ff8f73e658
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-08 15:00:23 +0100

    Edited selectorScene and created a controller for selector.

---
+++ src/main/java/nl/hu/coproco/controller/SelectorController.java
@@ -0,0 +1,16 @@
+package nl.hu.coproco.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Developed by Arjan.
+ */
+public class SelectorController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+}
