2ec1d3de8605713a86ad03bb106dd0b0e461f925
ArjanShoj <arjan.shojaeian@gmail.com>
2015-12-08 15:13:16 +0100

    All attributes are created for the view in the controller.

--- src/main/java/nl/hu/coproco/controller/EditorController.java
+++ src/main/java/nl/hu/coproco/controller/EditorController.java
@@ -5,7 +5,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +18,16 @@ import java.util.ResourceBundle;
 public class EditorController implements Initializable{
 
     @FXML private TextField namefield;
+    @FXML private ComboBox scopebox;
+    @FXML private ComboBox purposebox;
+    @FXML private TextField problemfield;
+    @FXML private TextField solutionfield;
+    @FXML private TextField consequencesfield;
 
+    @FXML private ImageView diagramfield;
+
+    @FXML private Button browsebutton;
+    @FXML private Button cancelbutton;
     @FXML private Button addbutton;
 
 
--- src/main/java/nl/hu/coproco/controller/SelectorController.java
+++ src/main/java/nl/hu/coproco/controller/SelectorController.java
@@ -1,6 +1,11 @@
 package nl.hu.coproco.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,8 +14,19 @@ import java.util.ResourceBundle;
  * Developed by Arjan.
  */
 public class SelectorController implements Initializable {
+
+    @FXML private ComboBox scopebox;
+    @FXML private ComboBox purposebox;
+    @FXML private ComboBox problembox;
+    @FXML private Label namefield;
+    @FXML private Label solutionfield;
+    @FXML private Label consequencesfield;
+    @FXML private ImageView diagramview;
+
+    @FXML private Button cancelbutton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
     }
 }
