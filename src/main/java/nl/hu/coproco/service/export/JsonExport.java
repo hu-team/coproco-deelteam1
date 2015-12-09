797a28843e33fcaf44e1e53be91e39ad42eb4932
Olivier <olivier.hendriks.limited@gmail.com>
2015-12-09 02:30:54 +0100

    Finished the JsonExport class, need some testing now

--- src/main/java/nl/hu/coproco/service/export/JsonExport.java
+++ src/main/java/nl/hu/coproco/service/export/JsonExport.java
@@ -6,11 +6,11 @@ import com.google.gson.JsonObject;
 import nl.hu.coproco.domain.PatternStorage;
 import nl.hu.coproco.domain.Pattern;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.ArrayList;
 
-
-
-import java.util.Arrays;
 
 
 public class JsonExport implements Export {
@@ -21,40 +21,40 @@ public class JsonExport implements Export {
     //Initializing new JsonArray
     private JsonArray jArry = new JsonArray();
 
-    @Override
+    //Main method used to call the other methods and use this class
     public boolean saveExport(String filename) {
-       if(fillJsonArray(filename)){
+       if(fillJsonArray()){
+           //Create the JSon output file
+           createJsonOutputFile(filename);
            return true;
        }else{return false;}
     }
 
-    public boolean fillJsonArray(String fileName){
+    protected boolean fillJsonArray(){
         //getting all patterns, should use the controller instead of speaking directly to patternStorage
         allPatterns = patternStorage.getPatterns();
-
-        //What do we use fileName for?
 
         //try catch to fill up the Json-array
         try
         {
             for (i=0;   i<=allPatterns.size();  i++)
             {
-                JsonObject jObjd = new JsonObject();
+                JsonObject jObj = new JsonObject();
                 //One JsonObject per Object in the ArrayList
 
-                jObjd.addProperty("Name", allPatterns.get(i).getName());
-                jObjd.addProperty("Problem", allPatterns.get(i).getProblem());
-                jObjd.addProperty("Solution", allPatterns.get(i).getSolution());
-                jObjd.addProperty("Consequences", allPatterns.get(i).getConsequences());
-                jObjd.addProperty("ScopeName", allPatterns.get(i).getScope().getName());
-                jObjd.addProperty("PurposeName", allPatterns.get(i).getPurpose().getName());
-                jObjd.addProperty("Diagram", allPatterns.get(i).getDiagram().getFilePath());
+                jObj.addProperty("Name", allPatterns.get(i).getName());
+                jObj.addProperty("Problem", allPatterns.get(i).getProblem());
+                jObj.addProperty("Solution", allPatterns.get(i).getSolution());
+                jObj.addProperty("Consequences", allPatterns.get(i).getConsequences());
+                jObj.addProperty("ScopeName", allPatterns.get(i).getScope().getName());
+                jObj.addProperty("PurposeName", allPatterns.get(i).getPurpose().getName());
+                jObj.addProperty("Diagram", allPatterns.get(i).getDiagram().getFilePath());
 
                 //This is optional, but could be useful
-                jObjd.addProperty("ClassName", allPatterns.get(i).getClass().getName());
+                jObj.addProperty("ClassName", allPatterns.get(i).getClass().getName());
 
                 //Adding to the array
-                jArry.add(jObjd);
+                jArry.add(jObj);
             }
         }
 
@@ -67,5 +67,24 @@ public class JsonExport implements Export {
         //If everything in the for loop has been written succesfully to the jArry it will return true,
         // so that it can be exported. You want to empty the jArry once you get an error to prevent duplicates.
         return true;
+    }
+
+    protected void createJsonOutputFile(String filename) {
+        //Creating the output JSon file
+        JsonObject outputFileJSon = new JsonObject();
+        outputFileJSon.addProperty("name", filename);
+        outputFileJSon.add("Patterns", jArry);
+
+        //Actually writing the data to a file
+        writeFile(outputFileJSon);
+    }
+
+    protected void writeFile(JsonObject outputFileJSon) {
+        try{
+            FileWriter exportFile = new FileWriter("/Users/Patterns.txt");
+            exportFile.write(outputFileJSon.toString());
+            System.out.println("Succesfully copied JSON Object to File...");
+        }
+        catch(IOException ex){}
     }
 }
