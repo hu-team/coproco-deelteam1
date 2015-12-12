12177bd8109312ece712d0610237626992b40955
Tom Valk <tomvalk@gmail.com>
2015-12-12 16:59:09 +0100

    Refactored + reintent jsonexport class.

--- src/main/java/nl/hu/coproco/service/export/JsonExport.java
+++ src/main/java/nl/hu/coproco/service/export/JsonExport.java
@@ -3,50 +3,52 @@ package nl.hu.coproco.service.export;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
+import nl.hu.coproco.Main;
 import nl.hu.coproco.domain.PatternStorage;
 import nl.hu.coproco.domain.Pattern;
 
 import java.io.IOException;
 
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 
 
 public class JsonExport implements Export {
-        private PatternStorage patternStorage;
-        private ArrayList<Pattern> allPatterns;
-        private int i = 0;
+    private PatternStorage patternStorage;
+    private ArrayList<Pattern> allPatterns;
+    private int i = 0;
 
-        //Initializing new JsonArray
-        private JsonArray jArry = new JsonArray();
+    // Initializing new JsonArray
+    private JsonArray jArry = new JsonArray();
 
-        //test function
+    // test function
     public JsonExport(PatternStorage PatternStorage){
-            patternStorage = PatternStorage;
+        patternStorage = PatternStorage;
     }
 
-    //Main method used to call the other methods and use this class
+    // Main method used to call the other methods and use this class
     public boolean saveExport(String filename) {
-       if(fillJsonArray()){
-           //Create the JSon output file
+        if(fillJsonArray()){
+            // Create the JSon output file
 
-           createJsonOutputFile(filename);
-           return true;
+            createJsonOutputFile(filename);
+            return true;
 
-       }else{return false;}
+        }else{return false;}
     }
 
     private boolean fillJsonArray(){
-        //getting all patterns, should use the controller instead of speaking directly to patternStorage
+        // getting all patterns, should use the controller instead of speaking directly to patternStorage
         allPatterns = patternStorage.getPatterns();
 
 
-        //try catch to fill up the Json-array
+        // try catch to fill up the Json-array
         try {
             for (i = 0; i < allPatterns.size(); i++) {
                 JsonObject jObj = new JsonObject();
-                //One JsonObject per Object in the ArrayList
+                // One JsonObject per Object in the ArrayList
 
                 jObj.addProperty("Name", allPatterns.get(i).getName());
                 jObj.addProperty("Problem", allPatterns.get(i).getProblem());
@@ -54,16 +56,12 @@ public class JsonExport implements Export {
                 jObj.addProperty("Consequences", allPatterns.get(i).getConsequences());
                 jObj.addProperty("ScopeName", allPatterns.get(i).getScope().getName());
                 jObj.addProperty("PurposeName", allPatterns.get(i).getPurpose().getName());
-//<<<<<<< HEAD
-                //jObj.addProperty("Diagram", allPatterns.get(i).getDiagram().getFilePath());
-//=======
-                //jObj.addProperty("Diagram", allPatterns.get(i).getDiagram().getEncodedImage());
-//>>>>>>> cef2b162376774694ccc53caf4af2be5fdc28bf8
+                jObj.addProperty("Diagram", allPatterns.get(i).getDiagram().getEncodedImage());
 
-                //This is optional, but could be useful
+                // This is optional, but could be useful when validating the class it was on the moment of exporting
                 jObj.addProperty("ClassName", allPatterns.get(i).getClass().getName());
 
-                //Adding to the array
+                // Adding to the array
                 jArry.add(jObj);
             }
         }
@@ -75,26 +73,30 @@ public class JsonExport implements Export {
             return false;
 
         }
-        //If everything in the for loop has been written succesfully to the jArry it will return true,
+        // If everything in the for loop has been written succesfully to the jArry it will return true,
         // so that it can be exported. You want to empty the jArry once you get an error to prevent duplicates.
         return true;
     }
 
     private void createJsonOutputFile(String filename) {
-        //Creating the output JSon file
+        // Creating the output JSon file
         JsonObject outputFileJSon = new JsonObject();
+
         outputFileJSon.addProperty("name", filename);
+        outputFileJSon.addProperty("build", Main.BUILD);
+        outputFileJSon.addProperty("version", Main.VERSION);
+
         outputFileJSon.add("Patterns", jArry);
 
-        //Actually writing the data to a file
+        // Actually writing the data to a file
         writeFile(outputFileJSon);
     }
 
     private void writeFile(JsonObject outputFileJSon) {
         try{
-            //Creating the actual file for the client. Path could contain variables to decide the path yourself, but we should have a String for it then
-            PrintWriter exportFile = new PrintWriter("Patterns.txt");
-            //Writing the JSon to text in this file
+            // Creating the actual file for the client. Path could contain variables to decide the path yourself, but we should have a String for it then
+            PrintWriter exportFile = new PrintWriter("Patterns.txt", "UTF-8");
+            // Writing the JSon to text in this file
 
             exportFile.write(outputFileJSon.toString());
             exportFile.close();
