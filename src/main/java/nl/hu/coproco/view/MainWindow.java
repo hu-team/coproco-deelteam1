7af570f83ab43866ba942eb81200947e757aae13
Tom Valk <tomvalk@gmail.com>
2015-12-12 17:09:47 +0100

    Adding confirm on clicking the close button.

--- src/main/java/nl/hu/coproco/view/MainWindow.java
+++ src/main/java/nl/hu/coproco/view/MainWindow.java
@@ -1,10 +1,16 @@
 package nl.hu.coproco.view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
 
 
 public class MainWindow extends Application {
@@ -26,6 +32,23 @@ public class MainWindow extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.sizeToScene();
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                // Don't allow closing it when not in other stage.
+
+                // Ask for exit
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Exit application");
+                alert.setContentText("Are you sure to exit the application and not export the patterns?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.CANCEL) {
+                    event.consume();
+                }
+            }
+        });
     }
 
     public void openWindow(String args[]) {
